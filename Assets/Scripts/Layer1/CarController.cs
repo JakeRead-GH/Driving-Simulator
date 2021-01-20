0520192110e43a5d04b9e14902539360169bbb99
Jake Read <jakeread99@gmail.com>
2021-01-20 17:09:10 -0500

    Added Mobile Controls

--- Assets/Scripts/Layer1/CarController.cs
+++ Assets/Scripts/Layer1/CarController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class CarController : MonoBehaviour
 {
@@ -14,12 +15,22 @@ public class CarController : MonoBehaviour
     public TextMeshProUGUI speedometer;
     public Material brakeLights;
     public Material reverseLights;
+    public Slider gearStick;
 
     private float horizontalInput;
     private float verticalInput;
     private float steeringAngle;
     private float brakes;
     private float inverseZ;
+    private float lastClick = 0f;
+    private float doubleClickInterval = 0.5f;
+
+    private string clickType;
+    private string gear;
+
+    private bool acceleratorPressed;
+    private bool brakePressed;
+    private bool usingKeyboard;
 
     // Assigned in Unity.
     public float maxSteerAngle;
@@ -29,6 +40,7 @@ public class CarController : MonoBehaviour
 
     private void Start()
     {
+        gear = "Drive";
         gameManager = GameObject.Find("GameManager");
     }
 
@@ -41,17 +53,38 @@ public class CarController : MonoBehaviour
             GetInput();
             Steer();
 
-            if (Input.GetKey(KeyCode.Space))
+            if (Input.GetKey(KeyCode.Space) || brakePressed)
             {
-                Brake();
+                if (clickType == "SingleClick")
+                {
+                    Accelerate(-1);
+                }
+                else
+                {
+                    Brake();
+                }
             }
-            else
+            else if (acceleratorPressed)
             {
-                Accelerate();
-            }
+                Accelerate(1);
+            }            
 
             UpdateWheelPoses();
             UpdateSpeed();
+        }
+    }
+
+    public void ChangeGear()
+    {
+        if (gearStick.value == 0)
+        {
+            gear = "Drive";
+            reverseLights.SetColor("_EmissionColor", Color.black);
+        }
+        else if (gearStick.value == 1)
+        {
+            gear = "Reverse";
+            reverseLights.SetColor("_EmissionColor", Color.red * 0.5f);
         }
     }
 
@@ -60,8 +93,58 @@ public class CarController : MonoBehaviour
        output value is any float between -1 and 1 based on how long the keys are held. */
     private void GetInput()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = SimpleInput.GetAxis("Horizontal");
+
+        if (SimpleInput.GetKey(KeyCode.W) || SimpleInput.GetKey(KeyCode.S))
+        {
+            usingKeyboard = true;
+            verticalInput = SimpleInput.GetAxis("Vertical");
+            Accelerate(verticalInput);
+        }
+        else if (usingKeyboard)
+        {
+            frontDriverWheel.motorTorque = 0;
+            frontPassengerWheel.motorTorque = 0;
+        }
+    }
+
+
+    public void AcceleratorPressed()
+    {
+        acceleratorPressed = true;
+        usingKeyboard = false;
+    }
+
+
+    public void BrakePressed()
+    {
+        brakePressed = true;
+        usingKeyboard = false;
+        brakeLights.SetColor("_EmissionColor", Color.red * 0.25f);
+
+        if ((lastClick + doubleClickInterval) > Time.time)
+        {
+            clickType = "DoubleClick";
+        }
+        else
+        {
+            lastClick = Time.time;
+            clickType = "SingleClick";
+        }
+
+        verticalInput = -1;
+    }
+
+
+    public void SpeedButtonReleased()
+    {
+        acceleratorPressed = false;
+        brakePressed = false;
+
+        brakeLights.SetColor("_EmissionColor", Color.black);
+
+        frontDriverWheel.motorTorque = 0;
+        frontPassengerWheel.motorTorque = 0;
     }
 
 
@@ -88,26 +171,54 @@ public class CarController : MonoBehaviour
         brakes = 20000;
         frontDriverWheel.brakeTorque = brakes;
         frontPassengerWheel.brakeTorque = brakes;
-        brakeLights.SetColor("_EmissionColor", Color.red * 0.25f);
     }
 
 
     // Changes the players acceleration to a max motor force of 50 based on user input.
-    private void Accelerate()
+    private void Accelerate(float acceleration)
     {
+        inverseZ = playerRB.transform.InverseTransformDirection(playerRB.velocity).z;
         frontDriverWheel.brakeTorque = 0;
         frontPassengerWheel.brakeTorque = 0;
-        frontDriverWheel.motorTorque = motorForce * verticalInput;
-        frontPassengerWheel.motorTorque = motorForce * verticalInput;
-        brakeLights.SetColor("_EmissionColor", Color.black);
 
-        inverseZ = playerRB.transform.InverseTransformDirection(playerRB.velocity).z;
+        if (gear == "Drive")
+        {
+            frontDriverWheel.motorTorque = motorForce * acceleration;
 
-        if (inverseZ < -1)
+            if (frontDriverWheel.motorTorque < 0 && inverseZ < 0 && usingKeyboard == false)
+            {
+                frontDriverWheel.motorTorque = 0;
+            }
+
+            frontPassengerWheel.motorTorque = motorForce * acceleration;
+
+            if (frontPassengerWheel.motorTorque < 0 && inverseZ < 0 && usingKeyboard == false)
+            {
+                frontPassengerWheel.motorTorque = 0;
+            }
+        }
+        else if (gear == "Reverse")
+        {
+            frontDriverWheel.motorTorque = (motorForce * acceleration * -1) / 2;
+
+            if (frontDriverWheel.motorTorque > 0 && inverseZ > 0)
+            {
+                frontDriverWheel.motorTorque = 0;
+            }
+
+            frontPassengerWheel.motorTorque = (motorForce * acceleration * -1) / 2;
+
+            if (frontPassengerWheel.motorTorque > 0 && inverseZ > 0)
+            {
+                frontPassengerWheel.motorTorque = 0;
+            }
+        }
+
+        if (inverseZ < - 0.1 && usingKeyboard)
         {
             reverseLights.SetColor("_EmissionColor", Color.red * 0.5f);
         }
-        else
+        else if (usingKeyboard)
         {
             reverseLights.SetColor("_EmissionColor", Color.black);
         }
